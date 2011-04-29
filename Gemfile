ba6f6d574dd158e85bf71b1694a3c24cd5c04a27
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-04-28 22:56:58 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source :rubygems
+
+# Project requirements
+gem 'rake'
+gem 'rack-flash'
+gem 'thin' # or mongrel
+
+# Component requirements
+gem 'haml'
+gem 'SystemTimer', :require => "system_timer"
+gem 'mongoid', "2.0.0"
+gem 'bson_ext', :require => "mongo"
+
+# Test requirements
+gem 'machinist', :group => 'test'
+gem 'machinist_mongo', :require => 'machinist/mongoid', :group => 'test'
+gem 'rr', :group => "test"
+gem 'riot', :group => "test"
+gem 'rack-test', :group => "test", :require => "rack/test"
+
+
+# Padrino
+gem 'padrino', "0.9.26"
---
+++ test/test_config.rb
@@ -0,0 +1,36 @@
+PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+require File.join(File.dirname(__FILE__),'blueprints')
+require 'riot/rr'
+
+# Specify your app using the #app helper inside a context.
+# Takes either an app class or a block argument.
+# app { Padrino.application }
+# app { Web.tap { |app| } }
+
+class Riot::Situation
+  include Rack::Test::Methods
+  ##
+  # You can handle all padrino applications using instead:
+  #   Padrino.application
+  # Or just the Application itself like:
+  #   Web.tap { |app|  }
+
+  def app
+    @app || Padrino.application
+  end
+end
+
+class Riot::Context
+  # Set the Rack app which is to be tested.
+  #
+  #   context "MyApp" do
+  #     app { [200, {}, "Hello!"] }
+  #     setup { get '/' }
+  #     asserts(:status).equals(200)
+  #   end
+  def app(app=nil, &block)
+    setup { @app = (app || block.call) }
+  end
+end
+
