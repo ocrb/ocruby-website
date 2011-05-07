5c442b411a1b4c467e5c45b8c41f11e6631b9a25
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-05-06 19:49:54 -0700

    [app/user] add email field as well as validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User
   field :first_name, :type => String
   field :last_name,  :type => String
   field :username,   :type => String
+  field :email,      :type => String
+
+  validates_presence_of   :username
+  validates_uniqueness_of :username, :case_sensitive => false
+  validates_length_of     :username, :within => 1..30, :on => :create
+  validates_format_of     :username, :with => %r{\w}
+
+  validates_presence_of   :email
+  validates_uniqueness_of :email,    :case_sensitive => false
+  validates_format_of     :email,    :with => %r{\w+@\w+\.\w+}
+  validates_length_of     :email,    :within => 4..320
 end
--- test/app/models/user_test.rb
+++ test/app/models/user_test.rb
@@ -12,5 +12,15 @@ context "User Model" do
     asserts_topic.has_field :last_name,  :type => String
     asserts_topic.has_field :username,   :type => String
     asserts_topic.has_field :email,      :type => String
+
+    asserts_topic.has_validation :validates_presence_of,   :username
+    asserts_topic.has_validation :validates_uniqueness_of, :username, :case_sensitive => false
+    asserts_topic.has_validation :validates_length_of,     :username, :within => 1..30, :on => :create
+    asserts_topic.has_validation :validates_form_of,       :username, :with => %r{\w}
+
+    asserts_topic.has_validation :validates_presence_of,   :email
+    asserts_topic.has_validation :validates_uniqueness_of, :email,    :case_sensitive => false
+    asserts_topic.has_validation :validates_format_of,     :email,    :with => %r{\w+@\w+\.\w+}
+    asserts_topic.has_validation :validates_length_of,     :email,    :within => 4..320
   end
 end
