6a1bdafbe5ed130cc24aec12c6ceac6188ad40be
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2011-05-18 19:27:03 -0700

    add initial project model

---
+++ app/models/project.rb
@@ -0,0 +1,11 @@
+class Project
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name,         :type => String
+  field :description,  :type => String
+
+  field :github_url,   :type => String
+  field :rubygems_url, :type => String
+  field :website_url,  :type => String
+end
