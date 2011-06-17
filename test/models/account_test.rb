ce1ada63805ffdf699b7609adb7e299959da415a
Brendon Murphy <xternal1+github@gmail.com>
2011-06-16 23:55:58 -0700

    Fixed requirement in account_test.

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
+require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
 context "Account Model" do
   context 'can be created' do
