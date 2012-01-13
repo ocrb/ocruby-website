d2969a075fb93986a3c7845f362b7eb31287ce26
Justin Beltran <justinvbeltran@gmail.com>
2012-01-12 21:07:39 -0800

    Replacing presentation_requests_controller_test with spec

---
+++ spec/app/controllers/presentation_requests_controller_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper.rb')
+
+describe PresentationRequest do
+  before do
+    DatabaseCleaner.clean
+    app Web
+  end
+
+  describe "GET :index" do
+    before do
+      2.times { PresentationRequest.make }
+      get "/presentation_requests"
+    end
+
+    it "returns a valid response" do
+      assert_includes last_response.body, PresentationRequest.first.content
+      assert_includes last_response.body, PresentationRequest.last.content
+
+      assert_equal 200, last_response.status
+    end
+  end
+end
