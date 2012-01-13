5e1e70ee6c9fca8b163a2029513b69bd18dd032f
Brendon Murphy <xternal1+github@gmail.com>
2012-01-12 23:02:37 -0800

    Minor spec cleanups.

--- spec/app/models/project_spec.rb
+++ spec/app/models/project_spec.rb
@@ -14,6 +14,8 @@ describe Project do
     assert_has_field project, :github_url,   :type => String
     assert_has_field project, :rubygems_url, :type => String
     assert_has_field project, :website_url,  :type => String
+
+    assert_has_association project, :referenced_in, :user
   end
 
 end
