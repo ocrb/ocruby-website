464f68866009aa77ea461955249492cce655355b
Brendon Murphy <xternal1+github@gmail.com>
2012-01-19 23:58:16 -0800

    reconfig mongo for production

--- config/database.rb
+++ config/database.rb
@@ -1,25 +1,7 @@
-
-# Connection.new takes host, port
-host = 'localhost'
-port = Mongo::Connection::DEFAULT_PORT
-
-database_name = case Padrino.env
-  when :development then 'web_development'
-  when :production  then 'web_production'
-  when :test        then 'web_test'
+Mongoid.configure do |config|
+  if Padrino.env == :production
+    config.master = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+  else
+    config.master = Mongo::Connection.new.db("web_#{Padrino.env}")
+  end
 end
-
-Mongoid.database = Mongo::Connection.new(host, port).db(database_name)
-
-# You can also configure Mongoid this way
-# Mongoid.configure do |config|
-#   name = @settings["database"]
-#   host = @settings["host"]
-#   config.master = Mongo::Connection.new.db(name)
-#   config.slaves = [
-#     Mongo::Connection.new(host, @settings["slave_one"]["port"], :slave_ok => true).db(name),
-#     Mongo::Connection.new(host, @settings["slave_two"]["port"], :slave_ok => true).db(name)
-#   ]
-# end
-#
-# More installation and setup notes are on http://mongoid.org/docs/
