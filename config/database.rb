d596167dfa17f5acbfc869480847435a7d6487ea
Brendon Murphy <xternal1+github@gmail.com>
2012-01-20 00:05:14 -0800

    no, really fixing db for heroku.

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,10 @@
+require 'uri'
+
 Mongoid.configure do |config|
   if Padrino.env == :production
-    config.master = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+    uri  = URI.parse(ENV['MONGOLAB_URI'])
+    conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+    config.master = conn.db(uri.path.gsub(/^\//, ''))
   else
     config.master = Mongo::Connection.new.db("web_#{Padrino.env}")
   end
