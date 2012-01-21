8b3c3c863c53f3951638266d38f938ad9c1b127f
Brendon Murphy <xternal1+github@gmail.com>
2012-01-20 20:04:13 -0800

    tweak meetup timeformat

--- app/helpers/meetup_helper.rb
+++ app/helpers/meetup_helper.rb
@@ -1,7 +1,7 @@
 Web.helpers do
   def meetup_summary(meetup)
     if meetup
-      time = (Time.at meetup.time.to_i / 1000).strftime("%b %d, %l:%M%p")
+      time = (Time.at meetup.time.to_i / 1000).strftime("%b %d %l:%M%p")
       link_to "#{meetup.name}, #{time} @ #{meetup.venue && meetup.venue.name}", meetup.event_url
     end
   end
